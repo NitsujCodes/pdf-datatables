af912f65699662334e349c11eb414bc4f10102bc
Justin Smith <justin_jt_smith@live.co.uk>
2025-02-05 00:29:22 +0000

    Some comments to keep track what things are going to be for

--- src/DTO/ColumnConfig.php
+++ src/DTO/ColumnConfig.php
@@ -4,6 +4,9 @@ namespace NitsujCodes\PDFDataTable\DTO;
 
 use NitsujCodes\PDFDataTable\DTO\Interfaces\IDTO;
 
+/**
+ * For PDF related settings
+ */
 class ColumnConfig extends BaseDTO implements IDTO
 {
 
--- src/DTO/RowConfig.php
+++ src/DTO/RowConfig.php
@@ -4,6 +4,9 @@ namespace NitsujCodes\PDFDataTable\DTO;
 
 use NitsujCodes\PDFDataTable\DTO\Interfaces\IDTO;
 
+/**
+ * For PDF related settings
+ */
 class RowConfig extends BaseDTO implements IDTO
 {
 
